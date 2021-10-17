c4ba3c9ce0437765f590004b341aaf0c0ef7f0b4
Kevin Ridings <kevinridings@hotmail.com>
2021-10-17 16:40:31 -0600

    Procedimientos programados

---
+++ Database/dbo/Tables/Distrito.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Distrito]
+(
+	IdDistrito INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Distrito PRIMARY KEY CLUSTERED(IdDistrito),
+	NombreDistrito VARCHAR(50) NULL,
+	IdCanton INT NOT NULL
+		CONSTRAINT FK_Distrito_Canton FOREIGN KEY(IdCanton) REFERENCES dbo.Canton(IdCanton)
+
+)WITH (DATA_COMPRESSION = PAGE)
+GO
+
+
+
