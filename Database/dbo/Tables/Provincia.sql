e5e9d24f25b96c94819fa3c27867e50143e24f3d
Admin <Admin@ADMIN-PC>
2021-10-17 15:18:11 -0600

    Add project files.

---
+++ Database/dbo/Tables/Provincia.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Provincia]
+(
+	[IdProvincia] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Provincia PRIMARY KEY CLUSTERED (IdProvincia)
+	,NombreProvincia VARCHAR(50) NOT NULL
+
+)
